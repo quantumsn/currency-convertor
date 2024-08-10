57edd839008ebdbab42d2fbb3829b5e60f917059
quantumsn <samprity.nayak9830@gmail.com>
2024-08-10 18:37:48 +0530

    Add Resposivness

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         backgroundImage: `url('https://plus.unsplash.com/premium_photo-1681487767138-ddf2d67b35c1?q=80&w=1555&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
       }}
     >
-      <div className="w-full">
+      <div className="w-full" style={{ margin: "1rem" }}>
         <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
           <form
             onSubmit={(e) => {
